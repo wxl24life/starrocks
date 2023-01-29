211b1563f70977562a0bfb9bec94c07ef93af5b4
yuzhou.zk <yuzhou.zk@alibaba-inc.com>
2023-01-29 08:05:56 +0000

    [EMR][Feature] Support get ip by host for stream load in some enviroment like k8s
    In order to adapt to the k8s environment, our BE registration logic currently uses hostname directly. However, this hostname cannot be recognized in the external environment, so an unknownhost exception like 'java.net.UnknownHostException: backend-1' will be thrown when importing data using stream load. This pr supports obtaining ip according to hostname and returning it to the client
            Link: https://code.alibaba-inc.com/soe/starrocks/codereview/11447359
    
        * [Feature] Support get ip by host for stream load in some enviroment like k8s

--- fe/fe-core/src/main/java/com/starrocks/http/rest/LoadAction.java
+++ fe/fe-core/src/main/java/com/starrocks/http/rest/LoadAction.java
@@ -35,6 +35,7 @@
 package com.starrocks.http.rest;
 
 import com.google.common.base.Strings;
+import com.starrocks.common.Config;
 import com.starrocks.common.DdlException;
 import com.starrocks.http.ActionController;
 import com.starrocks.http.BaseRequest;
@@ -50,9 +51,12 @@ import com.starrocks.warehouse.Warehouse;
 import io.netty.handler.codec.http.HttpHeaders;
 import io.netty.handler.codec.http.HttpMethod;
 import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.validator.routines.InetAddressValidator;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -130,7 +134,20 @@ public class LoadAction extends RestBaseAction {
             throw new DdlException("No backend or compute node alive.");
         }
 
-        TNetworkAddress redirectAddr = new TNetworkAddress(node.getHost(), node.getHttpPort());
+        String redirectHost = node.getHost();
+        if (Config.stream_load_force_use_ip) {
+            InetAddressValidator validator = InetAddressValidator.getInstance();
+            if (!validator.isValidInet4Address(redirectHost) && !validator.isValidInet6Address(redirectHost)) {
+                try {
+                    InetAddress host = InetAddress.getByName(redirectHost);
+                    redirectHost = host.getHostAddress();
+                } catch (UnknownHostException ex) {
+                    LOG.warn("get redirect host for be {} failed!", redirectHost);
+                }
+            }
+        }
+
+        TNetworkAddress redirectAddr = new TNetworkAddress(redirectHost, node.getHttpPort());
 
         LOG.info("redirect load action to destination={}, db: {}, tbl: {}, label: {}",
                 redirectAddr.toString(), dbName, tableName, label);
