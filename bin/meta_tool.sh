46d5a0d806e4fbe526a728a51e90278cb66ff32d
wangriyu.wry <wangriyu.wry@alibaba-inc.com>
2023-03-01 10:44:13 +0800

    [EMR][Feature] add a jindo config file to initialize JindoSDK         Link: https://code.alibaba-inc.com/soe/starrocks/codereview/11824013
        * [EMR][Feature] add a jindo config file to initialize JindoSDK

--- bin/meta_tool.sh
+++ bin/meta_tool.sh
@@ -20,6 +20,6 @@ source $STARROCKS_HOME/bin/common.sh
 jvmarch=`jvm_arch`
 export LD_LIBRARY_PATH=$STARROCKS_HOME/lib/jvm/$jvmarch/server:$STARROCKS_HOME/lib/jvm/$jvmarch:$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=$STARROCKS_HOME/lib/hadoop/native:$LD_LIBRARY_PATH
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$STARROCKS_HOME/lib/cachelib/lib64
+export LD_LIBRARY_PATH=$STARROCKS_HOME/lib/jindosdk:$LD_LIBRARY_PATH:$STARROCKS_HOME/lib/cachelib/lib64
 
 ${STARROCKS_HOME}/lib/starrocks_be meta_tool "$@"
