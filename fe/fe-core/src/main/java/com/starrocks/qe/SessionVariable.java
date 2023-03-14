475a80a5efcab776de97d4763583bf88842423be
yuzhou.zk <yuzhou.zk@alibaba-inc.com>
2023-03-14 14:08:44 +0800

    [EMR][Feature] Support to enable profile by config file of FE         Link: https://code.alibaba-inc.com/soe/starrocks/codereview/11979550
        * [EMR][Feature] Support to enable profile by config file of FE
            Link: https://code.alibaba-inc.com/soe/starrocks/codereview/11770741

--- fe/fe-core/src/main/java/com/starrocks/qe/SessionVariable.java
+++ fe/fe-core/src/main/java/com/starrocks/qe/SessionVariable.java
@@ -43,6 +43,7 @@ import com.google.common.reflect.TypeToken;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.ToNumberPolicy;
+import com.starrocks.common.Config;
 import com.starrocks.common.io.Text;
 import com.starrocks.common.io.Writable;
 import com.starrocks.common.util.CompressionUtils;
@@ -669,7 +670,7 @@ public class SessionVariable implements Serializable, Writable, Cloneable {
 
     // if true, need report to coordinator when plan fragment execute successfully.
     @VariableMgr.VarAttr(name = ENABLE_PROFILE, alias = IS_REPORT_SUCCESS)
-    private boolean enableProfile = false;
+    private boolean enableProfile = Config.profile_enable;
 
     // if true, will generate profile when load finished
     @VariableMgr.VarAttr(name = ENABLE_LOAD_PROFILE)
