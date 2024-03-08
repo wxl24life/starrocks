16b079207a830a3f5984f8057147a4663de240bb
drake_wang <wxl250059@alibaba-inc.com>
2024-03-08 14:44:26 +0800

    fix

--- be/src/storage/lake/staros_cache_stats_collector.cpp
+++ be/src/storage/lake/staros_cache_stats_collector.cpp
@@ -43,7 +43,7 @@ StatusOr<int64_t> calculate_cache_size(std::vector<std::string> paths) {
     }
 
     staros::starlet::fslib::FileSystem* fs = fs_st.value().get();
-    CacheStatCollector* collector = CacheStatCollector::instance(fs);
+    auto collector = CacheStatCollector::instance(fs);
     absl::StatusOr<int64_t> size_st = collector->collect_cache_size(paths);
     if (!size_st.ok()) {
         return to_status(size_st.status());
