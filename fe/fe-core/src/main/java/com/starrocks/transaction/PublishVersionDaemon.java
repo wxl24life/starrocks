704c4cbeb238fab869f743974b60749070901aed
lixianhai.lxh <lixianhai.lxh@alibaba-inc.com>
2024-12-12 17:43:58 +0800

    [Backport][BugFix] Publish time should be update in shared data (#53209)
    * update publish time in shared data
    
    Signed-off-by: smartlxh <smartlxh@gmail.com>

--- fe/fe-core/src/main/java/com/starrocks/transaction/PublishVersionDaemon.java
+++ fe/fe-core/src/main/java/com/starrocks/transaction/PublishVersionDaemon.java
@@ -408,6 +408,7 @@ public class PublishVersionDaemon extends FrontendDaemon {
                     continue;
                 }
                 publishingTransactions.add(txnId);
+                txnState.updateSendTaskTime();
                 CompletableFuture<Void> future = publishLakeTransactionAsync(txnState);
                 future.thenRun(() -> publishingTransactions.remove(txnId));
             }
@@ -433,6 +434,7 @@ public class PublishVersionDaemon extends FrontendDaemon {
                 List<Long> tableIdList = state.getTableIdList();
                 if (tableIdList.stream().noneMatch(publishingLakeTransactionsBatchTableId::contains)) {
                     publishingLakeTransactionsBatchTableId.addAll(tableIdList);
+                    state.updateSendTaskTime();
                     CompletableFuture<Void> future = publishLakeTransactionAsync(state);
                     future.thenRun(() -> tableIdList.forEach(publishingLakeTransactionsBatchTableId::remove));
                 }
@@ -458,7 +460,7 @@ public class PublishVersionDaemon extends FrontendDaemon {
                         continue;
                     }
                     publishingLakeTransactionsBatchTableId.add(tableId);
-
+                    txnStateBatch.updateSendTaskTime();
                     CompletableFuture<Void> future = publishLakeTransactionBatchAsync(txnStateBatch);
                     future.thenRun(() -> publishingLakeTransactionsBatchTableId.remove(tableId));
                 }
@@ -782,6 +784,7 @@ public class PublishVersionDaemon extends FrontendDaemon {
             if (success) {
                 try {
                     states.forEach(TransactionState::updatePublishTaskFinishTime);
+                    txnStateBatch.updatePublishTaskFinishTime();
                     globalTransactionMgr.finishTransactionBatch(dbId, txnStateBatch, null);
                     // here create the job to drop txnLog, for the visibleVersion has been updated
                     submitDeleteTxnLogJob(txnStateBatch);
