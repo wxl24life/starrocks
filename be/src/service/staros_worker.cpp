32cee0ac152da327a0e3ec649a4126c4ae16b7ac
zqy455486 <zqy455486@alibaba-inc.com>
2025-05-08 17:22:54 +0800

    [Stella][Enhancement] Support tablet meta cache
    Link: https://code.alibaba-inc.com/soe/stella/codereview/21491897

--- be/src/service/staros_worker.cpp
+++ be/src/service/staros_worker.cpp
@@ -549,6 +549,8 @@ void init_staros_worker(const std::shared_ptr<starcache::StarCache>& star_cache)
     fslib::FLAGS_index_cache_disk_size_bytes = config::starlet_index_cache_disk_size_bytes;
     fslib::FLAGS_index_cache_block_size_bytes = config::starlet_index_cache_block_size_bytes;
 
+    fslib::FLAGS_star_cache_meta_size_bytes = config::starlet_star_cache_meta_size_bytes;
+
     staros::starlet::StarletConfig starlet_config;
     starlet_config.rpc_port = config::starlet_port;
     g_worker = std::make_shared<StarOSWorker>();
