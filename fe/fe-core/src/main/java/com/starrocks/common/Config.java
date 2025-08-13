45c9721dea21b363c9fd7dbecd98fde2a8987152
yuzhou.zk <yuzhou.zk@alibaba-inc.com>
2025-08-13 12:41:49 +0800

    [Stella][Enhancement] make checkpoint interval configurable and print time cost for daemon Link: https://code.alibaba-inc.com/soe/stella/codereview/22939893 * [Stella][Enhancement] make checkpoint interval configurable and print time cost for daemon
    * use debug log

--- fe/fe-core/src/main/java/com/starrocks/common/Config.java
+++ fe/fe-core/src/main/java/com/starrocks/common/Config.java
@@ -3598,6 +3598,12 @@ public class Config extends ConfigBase {
     public static int deploy_serialization_core_pool_size = 1;
 
     /**
+     * Checkpoint interval, max limit will be 1800 seconds
+     */
+    @ConfField
+    public static int checkpoint_interval_second = 60;
+
+    /**
      * Enable auto create tablet when creating table and add partition
      **/
     @ConfField(mutable = true)
